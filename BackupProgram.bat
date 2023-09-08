d8dc03debebf1bc66393f3a7eac2338a20bdc8fe
Rexturnull <tomFP6H@gmail.com>
2023-09-09 03:48:30 +0800

    1120909 init

---
+++ BackupProgram.bat
@@ -0,0 +1,59 @@
+@echo off
+setlocal enabledelayedexpansion
+
+color 0a
+
+
+
+echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo "______            _                 ______                                    " 
+echo "| ___ \          | |                | ___ \                                   " 
+echo "| |_/ / __ _  ___| | ___   _ _ __   | |_/ / __ ___   __ _ _ __ __ _ _ __ ___  " 
+echo "| ___ \/ _\`|/ __| |/ / | | | '_ \  |  __/ '__/ _ \ / _\`| '__/ _\`| '_ \`_  \" 
+echo "| |_/ / (_| | (__|   <| |_| | |_) | | |  | | | (_) | (_| | | | (_| | | | | | |" 
+echo "\____/ \__,_|\___|_|\_\\__,_| .__/  \_|  |_|  \___/ \__, |_|  \__,_|_| |_| |_|" 
+echo "                            | |                      __/ |                    " 
+echo "                            |_|                     |___/                     " 
+echo "                                                                              " 
+echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo .
+echo .
+echo 1) You should put this bat file on the source directory!!!.
+echo 2) Enter the target Destination to Start Backup Program.
+echo 3) Enter 'exit' to Exit the program.
+echo .
+
+:inputLoop
+echo Current directory : %CD%
+echo .
+set /p targetDestination=Enter the target Destination (e.g., C:/): 
+
+if "!targetDestination!"=="" (
+    echo Target path is required. Please enter a valid target path.
+    goto inputLoop
+)
+
+if "!targetDestination!"=="exit" (
+    echo Exiting the Backup program... Bye~~~
+	exit /b
+)
+
+if not exist "!targetDestination!\" (
+    echo The specified path does not exist. Please enter a valid path.
+	echo .
+    goto inputLoop
+)
+echo .
+echo ===== Start your backup at %date% %time% =====
+echo .
+echo %CD% back up to !targetDestination!...
+xcopy /D/S/Y/E/C/I/R/A *.* !targetDestination!
+echo ===== End Differential backup at %date% %time% =====
+echo Enter 'exit' to Exit the program.
+echo .
+
+goto inputLoop
+
+
+
+PAUSE
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BackupProgram
 This is a small DOS program to help me quickly perform data differential backup.
+
+---
+
+1. Place this bat file in the source directory.
+2. Enter the destination directory for the backup.
+3. Enter "exit" to exit program.
+
+
+
+
+
